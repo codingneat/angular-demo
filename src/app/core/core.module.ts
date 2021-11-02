e79e71d9c00c91c0c49874f2889da6e759a63191
Daniel Vergara <daniel.vergara.echeverri@gmail.com>
2021-11-02 18:03:32 -0500

    feat: initial setup

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [HttpClientModule, RouterModule],
+})
+export class CoreModule {}
